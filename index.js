f55f8bcc4b0cae98654134762d4823696fc71272
kabornblack <shaibu.kabiru@email.com>
2022-08-02 08:35:36 +0100

    final piece

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ image1.setAttribute("src", randomImageSource);
 var randomNumber2 = match.floor(match.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 ducoment.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
