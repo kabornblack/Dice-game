f77c708a6e680884a566b676a65011069ca65703
kabornblack <shaibu.kabiru@email.com>
2022-08-02 08:09:01 +0100

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+var randomNumber1 = match.floor(match.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = match.floor(match.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+ducoment.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
